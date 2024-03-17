6dbc921b013ff7876115dad2af22b48954814bad
ADT1 <152813453+ANUDT@users.noreply.github.com>
2024-03-17 19:46:06 +0000

    license type and links

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -2,6 +2,12 @@ function generateMarkdown(data){
  return `# ${data.title}`
  }
 
+ function renderLicenseLink(license) {
+  const licenseLinks = {
+        'MIT': '[MIT License](https://opensource.org/licenses/MIT)',
+        'Apache-2.0': '[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)',
+        'GPL-3.0': '[GNU General Public License v3.0](https://opensource.org/licenses/GPL-3.0)'
+         };
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
