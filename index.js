696e91a359cbdab7e41761ac23d673918b1a351a
ADT1 <152813453+ANUDT@users.noreply.github.com>
2024-03-17 19:05:19 +0000

    index js: function writeToFile, function will write data to file synchronoously, using filename provides in fileName and Data

--- index.js
+++ index.js
@@ -3,22 +3,13 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 const questions = require(`./utils/questions`);
 
-function writeFile(markdown) {
-
-    fs.writeFile(`readme.md`, markdown(error) => {
-        if(error) {
-            console.log(error);
-        else {
-            console.log(`created index.html succesfully`);
-        }
-    });
+function writeToFile(fileName, data) {
+return fs.writeToFileSync (path.join(process.cwd(), fileName, data));
 }
 
 function init() {
 
-    inquirer
-        .prompt(questions)
-        .then({answers}) =>
+    inquirer.prompt(questions).then({Responses}) => {
     const markdown = generateMarkdown(answers);
     writeFile(markdown);
 });
