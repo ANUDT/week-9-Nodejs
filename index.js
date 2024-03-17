d498816016ab1c01b2ca8c71e2afbc93de446696
ADT1 <152813453+ANUDT@users.noreply.github.com>
2024-03-17 15:30:55 +0000

    index.js: function writeFile added : function: this will take the markdown file and write named readme.md

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const inquirer = require("inquirer");
 
 const generateMarkdown = require("./utils/generateMarkdown");
 const questions = require(`./utils/questions`);
+
+function writeFile(markdown) {
+
+    fs.writeFile(`readme.md`, markdown(error) => {
+        if(error) {
+            console.log(error);
+        else {
+            console.log(`created index.html succesfully`);
+        }
+    });
+}
