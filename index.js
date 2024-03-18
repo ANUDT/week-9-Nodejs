b98dcc9e6ad8e17a30db1fe23d16a50e7bd7396c
ADT1 <152813453+ANUDT@users.noreply.github.com>
2024-03-18 22:08:04 +0000

    modified index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
 
-const generateMarkdown = require("./utils/generateMarkdown");
-const questions = require(`./utils/questions`);
+const generateMarkdown = require("./generateMarkdown");
+const questions = require(`./questions`);
 
 function writeToFile(fileName, data) {
-return fs.writeToFileSync (path.join(process.cwd(), fileName, data));
+
+    return fs.writeFileSync (fileName, data);
 }
 
 function init() {
 
     inquirer.prompt(questions).then((responses) =>{
-          console.log ("README.md generator File");
-        const markdown = generateMarkdown(Responses);
-    writeToFile("./README.md", generateMarkdown({ ...responses}))
+        console.log ("README.md generator File");
+        const markdown = generateMarkdown(responses);
+        writeToFile("README.md", markdown);
     })}
+
 init();
