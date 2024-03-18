605ec3d648278f76f93c72ff6208e59bd9f35bbe
ADT1 <152813453+ANUDT@users.noreply.github.com>
2024-03-18 22:03:41 +0000

    amended Let licenselink in generate markdownjs

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -2,26 +2,29 @@ function generateMarkdown(data){
  return `# ${data.title}`
  }
 
+ 
  function renderLicenseBadge(license) {
-  let licenseBadge = ""
+
+  let licenseLink = ""
   switch(license) {
     case 'MIT':
-      licenseBadge = `![Static Badge](https://img.shields.io/badge/license-MIT-blue)`
+      licenseLink = `![Static Badge](https://img.shields.io/badge/license-MIT-blue)`
       break;
      case 'APACHE 2.0':
-      licenseBadge = `![Static Badge](https://img.shields.io/badge/license/apache-2.0-blue)`
+      licenseLink = `![Static Badge](https://img.shields.io/badge/license/apache-2.0-blue)`
       break;
     case 'GPL 3.0':
-      licenseBadge = `![Static Badge](https://img.shields.io/badge/license-GPLv3-blue)`
+      licenseLink = `![Static Badge](https://img.shields.io/badge/license-GPLv3-blue)`
       break;
     case 'BSD 3':
-      licenseBadge = `![Static Badge](https://img.shields.io/badge/BSD-3-Purple)`
+      licenseLink = `![Static Badge](https://img.shields.io/badge/BSD-3-Purple)`
       break;
     case 'None':
-      licenseBadge = `No License Used`
+      licenseLink = `No License Used`
       break;
   }
-  return licenseBadge
+  return licenseLink
+}
 
 function generateMarkdown(data) {
   return `# ${data.title}
