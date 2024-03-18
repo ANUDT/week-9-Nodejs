8fc6105001d419120913a3e921f7fb7530113821
ADT1 <152813453+ANUDT@users.noreply.github.com>
2024-03-18 22:09:41 +0000

    modified and amended questions,js

--- questions.js
+++ questions.js
@@ -1,45 +1,39 @@
 const questions = [
     {
-    type: "input",
-    name: "##Your Project Title",
-    message: "project name",
+        type: "input",
+        name: "project_name",
+        message: "Enter the name of your project",
     },
-
     {
-    type: "input",
-    name: "##Description",
-    message: "functionality and purpose of this project",
+        type: "input",
+        name: "description",
+        message: "Functionality and purpose of this project",
     },
-    
     {
-    type: "input",
-    name: "##Table of content",
-    message: "project contents listed",
+        type: "input",
+        name: "installation",
+        message: "steps required to install",
     },
-
     {
-    type: "input",
-    name: "##Installation",
-    message: "steps required to install",
+        type: "input",
+        name: "usage",
+        message: "provide instructions on how to run this application",
     },
-
     {
-    type: "input",
-    name: "##Usage",
-    message: "provide instructions and examples for use, screenshots",
+        type: "input",
+        name: "credits",
+        message: "provide list of collaborators, Github profiles. third-party assets",
     },
-
     {
-    type: "input",
-    name: "##Credits",
-    message: "provide list of collaborators, Github profiles. third-party assets",
+        type: "list",
+        name: "licence",
+        message: "Select a type of license for your project",
+        choices: [`MIT`, `APACHE 2.0`, `CPL 3.0`, `none`]
     },
-
     {
-    type: "List",
-    name: "##Licence",
-    message: "provide high-quality README file",
-    choices: [`MIT`, `APACHE 2.0`, `CPL 3.0`, `none`].
+        type: "input",
+        name: "github",
+        message: "Enter your Github username",
     },
 ];
 
